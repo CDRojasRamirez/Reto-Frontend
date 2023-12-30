84f6d19d6a91fcc323de938817cbeb4236969e36
CDRojasRamirez <cdanielrap@gmail.com>
2023-12-30 15:10:32 -0500

    add overflow-hidden

--- src/components/FilterList/FilterListStyles.ts
+++ src/components/FilterList/FilterListStyles.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const FilterListHeader = styled.header`
   width: 100%;
+  overflow: hidden;
   background-color: #232323;
   position: fixed;
   z-index: 999;
